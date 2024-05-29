d5ff1239bd5a9ca65226fc3933d022d2b1bd0f8c
Henrique Walace <henriquewalace@hotmail.com>
2024-05-29 11:48:05 -0300

    test: Adiciona teste das classes Iphone e Nokia

--- SmartphoneOO/Program.cs
+++ SmartphoneOO/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Testando a herança e o polimorfismo das classes Nokia e Iphone
+
+using SmartphoneOO.Modelos;
+
+Nokia nokia = new Nokia(numero: "11999999999", modelo: "Modelo 1", imei: "123456789", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Iphone iphone = new Iphone(numero: "11899999999", modelo: "Modelo 2", imei: "1234543239", memoria: 256);
+iphone.ReceberLigação();
+iphone.InstalarAplicativo("Telegram");
